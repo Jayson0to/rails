c70cbe0474fcff67b48baba27ab6b68009cee452
Jayson0to <orui1912@gmail.com>
2023-05-04 19:22:17 +0900

    first

---
+++ app/controllers/concerns/post.rb
@@ -0,0 +1,21 @@
+class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :start_day, presence: true
+  validates :end_day, presence: true
+  validates :title, length: { maximum: 20 }
+  validates :schedule, length: { maximum: 500 }
+  validate :days, on: :create
+
+  def days
+    if start_day != nil
+      unless start_day.blank?
+        
+      end
+    if start_day > end_day
+      errors.add(:end_day,"は開始日より前の日付で登録できません。")
+    end
+    
+
+    end
+  end
+end
