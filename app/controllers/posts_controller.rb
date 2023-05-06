90942f23a5cdc0cfa1151a1370060c05a53920d9
Jayson0to <orui1912@gmail.com>
2023-05-06 14:35:46 +0900

    デバッガ削除、バリデーション移動、インデント整理をしました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,41 +7,45 @@ class PostsController < ApplicationController
     @post = Post.new
   end
  
-   def create
-    @post = Post.new(params.require(:post).permit(:title, :start_day, :end_day, :allday,:schedule))
+  def create
+    @post = Post.new(post_params)
     if @post.save
-      binding.pry
       flash[:notice] = "スケジュール登録しました。"
       redirect_to :posts
     else
       flash[:notice] = "登録に失敗しました。"
       render :new,status: :unprocessable_entity
     end
-   end
+ end
  
-   def show
+  def show
     @post = Post.find(params[:id])
-   end
+  end
  
-   def edit
+  def edit
     @post = Post.find(params[:id])
-   end
+  end
  
-   def update
+  def update
     @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:title, :start_day, :end_day, :allday, :schedule))
+    if @post.update(post_params)
       flash[:notice] = "更新しました"
       redirect_to :posts
     else
       flash[:notice] = "編集に失敗しました。"
       render :edit,status: :unprocessable_entity
     end
-    end
+  end
 
-    def destroy
-      @post = Post.find(params[:id])
-      @post.destroy
-      flash[:notice] = "予定を削除しました"
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "予定を削除しました"
       redirect_to :posts,status: :see_other
     end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :start_day, :end_day, :allday,:schedule)
   end
+end
